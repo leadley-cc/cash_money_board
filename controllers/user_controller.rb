458a30a4070c0568afe04ec6dcc25d109671c322
Michael Leadley <leadley75@gmail.com>
2017-08-29 09:43:02 +0100

    Complete user_controller

--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -7,7 +7,32 @@ get '/users' do
   erb(:user_index)
 end
 
+get '/users/new' do
+  erb(:user_new)
+end
+
+get '/users/:id/edit' do
+  @user = User.find(params["id"])
+  erb(:user_edit)
+end
+
 get '/users/:id' do
   @user = User.find(params["id"])
   erb(:user_show)
+end
+
+post '/users' do
+  User.new(params).save
+  redirect to "/users/#{params["id"]}"
+end
+
+post '/users/:id' do
+  User.new(params).save
+  redirect to "/users/#{params["id"]}"
+end
+
+post '/users/:id/delete' do
+  user = User.find(params["id"])
+  user.delete
+  redirect to "/users"
 end
