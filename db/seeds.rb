7150f912d015d1d638fef7bdb572037489bad981
Michael Leadley <leadley75@gmail.com>
2017-08-29 13:10:00 +0100

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,21 @@
 require "pry"
 
-require_relative "../models/tag"
-require_relative "../models/merchant"
 require_relative "../models/transaction"
+require_relative "../models/merchant"
+require_relative "../models/tag"
+require_relative "../models/user"
 
 Transaction.delete_all
 Merchant.delete_all
 Tag.delete_all
 User.delete_all
 
+## USERS
+
 michael = User.new({
   "first_name" => "Michael",
   "last_name" => "Leadley",
-  "budget_cap" => 20000
+  "budget_cap" => 25000
 })
 michael.save
 
@@ -30,17 +33,38 @@ jim = User.new({
 })
 jim.save
 
+theo = User.new({
+  "first_name" => "Theodore",
+  "last_name" => "Hev",
+  "budget_cap" => 240000
+})
+theo.save
+
+gert = User.new({
+  "first_name" => "Gertrude",
+  "last_name" => "Blenkinsop",
+  "budget_cap" => 420
+})
+gert.save
+
+## TAGS
+
 supermarket = Tag.new({"name" => "Supermarket"})
 supermarket.save
 
 food_drink = Tag.new({"name" => "Food & Drink"})
 food_drink.save
 
-tesco = Merchant.new({
-  "name" => "Tesco",
-  "tag_id" => supermarket.id
-})
-tesco.save
+bills = Tag.new({"name" => "Bills"})
+bills.save
+
+entertainment = Tag.new({"name" => "Entertainment"})
+entertainment.save
+
+transport = Tag.new({"name" => "Transport"})
+transport.save
+
+## MERCHANTS
 
 sains = Merchant.new({
   "name" => "Sainsbury's",
@@ -54,8 +78,34 @@ chanter = Merchant.new({
 })
 chanter.save
 
+sneakies = Merchant.new({
+  "name" => "Sneaky Pete's",
+  "tag_id" => entertainment.id
+})
+sneakies.save
+
+lidl = Merchant.new({
+  "name" => "Lidl",
+  "tag_id" => supermarket.id
+})
+lidl.save
+
+sky = Merchant.new({
+  "name" => "Sky",
+  "tag_id" => bills.id
+})
+sky.save
+
+buses = Merchant.new({
+  "name" => "Lothian Buses",
+  "tag_id" => transport.id
+})
+buses.save
+
+## TRANSACTIONS
+
 michael.new_transaction({
-  "value" => 2500,
+  "value" => 2385,
   "date_time" => Time.now,
   "merchant_id" => sains.id
 })
@@ -63,13 +113,13 @@ michael.new_transaction({
 michael.new_transaction({
   "value" => 1500,
   "date_time" => Time.now,
-  "merchant_id" => tesco.id
+  "merchant_id" => lidl.id
 })
 
-michael.new_transaction({
-  "value" => 500,
+sandra.new_transaction({
+  "value" => 1050,
   "date_time" => Time.now,
-  "merchant_id" => sains.id
+  "merchant_id" => lidl.id
 })
 
 michael.new_transaction({
@@ -82,6 +132,18 @@ jim.new_transaction({
   "value" => 235,
   "date_time" => Time.now,
   "merchant_id" => sains.id
+})
+
+michael.new_transaction({
+  "value" => 500,
+  "date_time" => Time.now,
+  "merchant_id" => sneakies.id
+})
+
+michael.new_transaction({
+  "value" => 1120,
+  "date_time" => Time.now,
+  "merchant_id" => buses.id
 })
 
 binding.pry
