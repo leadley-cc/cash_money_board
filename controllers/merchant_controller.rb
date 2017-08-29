c4461c5a461cb7ac026d1ae9615771527780c7a0
Michael Leadley <leadley75@gmail.com>
2017-08-29 10:13:16 +0100

    Complete merchant_controller

--- controllers/merchant_controller.rb
+++ controllers/merchant_controller.rb
@@ -7,7 +7,34 @@ get '/merchants' do
   erb(:merchant_index)
 end
 
+get '/merchants/new' do
+  @tags = Tag.all
+  erb(:merchant_new)
+end
+
+get '/merchants/:id/edit' do
+  @merchant = Merchant.find(params["id"])
+  @tags = Tag.all
+  erb(:merchant_edit)
+end
+
 get '/merchants/:id' do
   @merchant = Merchant.find(params[:id])
   erb(:merchant_show)
+end
+
+post '/merchants' do
+  Merchant.new(params).save
+  redirect to "/merchants"
+end
+
+post '/merchants/:id' do
+  Merchant.new(params).save
+  redirect to "/merchants/#{params["id"]}"
+end
+
+post '/merchants/:id/delete' do
+  merchant = Merchant.find(params["id"])
+  merchant.delete
+  redirect to "/merchants"
 end
