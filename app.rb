85792379b333b1e2727987293dc8021c32f71880
Michael Leadley <leadley75@gmail.com>
2017-10-04 18:36:01 +0100

    Correct for Heroku deployment

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require_relative "controllers/user_controller"
 require_relative "controllers/transaction_controller"
 require_relative "controllers/merchant_controller"
 require_relative "controllers/tag_controller"
-also_reload "models/*"
+# also_reload "models/*"
 
 get '/?' do
   erb(:index)
